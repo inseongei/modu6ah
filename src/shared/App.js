4947af24cc3d83447d4c249270713a2c1af85165
Maiowol <103277726+Maiowol@users.noreply.github.com>
2022-06-28 09:27:33 +0900

    타이틀 부분 수정 완료

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,4 +1,3 @@
-import React, {useState} from "react";
 import { Routes, Route } from 'react-router-dom';
 
 // pages
@@ -8,10 +7,10 @@ import SignUp from "../pages/SignUp";
 import Recruit from "../pages/Recruit";
 import Place from "../pages/Place";
 import Review from "../pages/Review";
-import Add1 from "../pages/Add1";
-import Add2 from "../pages/Add2";
-import Detail1 from "../pages/Detail1";
-import Detail2 from "../pages/Detail2"; 
+import AddOne from "../pages/AddOne";
+import AddTwo from "../pages/AddTwo";
+import DetailOne from "../pages/DetailOne";
+import DetailTwo from "../pages/DetailTwo"; 
 import Mypage from "../pages/MyPage";
 import ProfileManager from "../pages/ProfileManager";
 import BookMark from "../pages/BookMark";
@@ -22,21 +21,21 @@ function App() {
   return (
     <>
     <Routes>
-      <Route exact path="/" element={< Main/>} />
-      <Route exact path="/login" element={< LogIn />} />
-      <Route exact path="/signup" element={< SignUp />} />     
-      <Route exact path="/recruit" element={< Recruit />} /> 
-      <Route exact path="/place" element={< Place />} /> 
-      <Route exact path="/review" element={< Review />} /> 
-      <Route exact path="/add1" element={< Add1 />} />
-      <Route exact path="/add2" element={< Add2 />} />
-      <Route exact path="/detail1" element={< Detail1 />} />
-      <Route exact path="/detail2" element={< Detail2 />} />
-      <Route exact path="/mypage" element={< Mypage />} />
-      <Route exact path="/manager" element={< ProfileManager />} />
-      <Route exact path="/bookmark" element={< BookMark />} />
-      <Route exact path="/chatlist" element={< ChatList />} />
-      <Route exact path="/mainchat" element={< MainChat />} />
+      <Route  path="/" element={< DetailOne/>} />
+      <Route  path="/login" element={< LogIn />} />
+      <Route  path="/signup" element={< SignUp />} />     
+      <Route  path="/recruit" element={< Recruit />} /> 
+      <Route  path="/place" element={< Place />} /> 
+      <Route  path="/review" element={< Review />} /> 
+      <Route  path="/addone" element={< AddOne />} />
+      <Route  path="/addtwo" element={< AddTwo />} />
+      <Route  path="/detailone" element={< DetailOne />} />
+      <Route  path="/detailtwo" element={< DetailTwo />} />
+      <Route  path="/mypage" element={< Mypage />} />
+      <Route  path="/manager" element={< ProfileManager />} />
+      <Route  path="/bookmark" element={< BookMark />} />
+      <Route  path="/chatlist" element={< ChatList />} />
+      <Route  path="/mainchat" element={< MainChat />} />
     </Routes>
     </>
   );
