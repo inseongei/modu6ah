83209e7316ef84199f426a652dae63d31599520d
inseong <twin2688@naver.com>
2022-06-30 12:33:36 +0900

    footer : 모바일처리 / 아이콘 링크 등록완료

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,72 +1,91 @@
 import React from 'react'
 import styled from 'styled-components'
-import { FaReact,FaNodeJs } from "react-icons/fa";
-import { ImPencil } from "react-icons/im";
+import { FaFigma,FaGithub,FaStore } from "react-icons/fa";
+import '../shared/App.css'
+
+
+
 
 const Footer = () => {
   return (
     <FooterContainer>
-        <div className='FooterC'>
+        <div className='Footer_main'>
+            <div className='icon_box'>
+            <a href='https://github.com/Maiowol/Forkids_Project'><FaGithub></FaGithub></a> &nbsp;
+            <a href='https://github.com/changmin97/forkidsProject'><FaGithub></FaGithub></a> &nbsp;
+            <a href='https://www.figma.com/file/6oxe17NH1VuhHdZxdj9X9N/항해99'><FaFigma></FaFigma></a> &nbsp;
+            <a href='##'><FaStore></FaStore></a> &nbsp;
+            
+            </div>
 
-        <div className='icon'>
-            <FaReact className='react'></FaReact> X <FaNodeJs className='Node'></FaNodeJs> X <ImPencil className='design'></ImPencil>
-        </div>
+            <div>
+                Front-End : 김숙영, 정인성
+            </div>
 
-        <div className='footerInformation'>
-        <div>Back-End : 이창민, 조세림 , 안재훈</div>
-        <div>Front-End : 김숙영 , 정인성</div>
-        <div>designer : 서혜빈</div>
-        </div>
-        <div className='mine'> Copyright 2022. 모두의 육아 all rights reserved.</div>
+            <div className='position'>
+                Back-End : 이창민, 조세림,안재훈
+            </div>
+
+            <div>
+                designer : 서혜빈
+            </div>
+            <span className='mine'>
+                Copyright 2022. 모두의육아 all right reserved.
+            </span>
         </div>
     </FooterContainer>
   )
 }
 
 const FooterContainer = styled.footer`
-@font-face {
-    font-family: 'IBMPlexSansKR-Regular';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-07@1.0/IBMPlexSansKR-Regular.woff') format('woff');
-    font-weight: normal;
-    font-style: normal;
-}
-    font-family: 'IBMPlexSansKR-Regular';
     width: 100%; 
-    color:#e0e0e0;
-    height: 30vh;
-    background-color: #37474f;
+    color:#3C3C3C;
+    height: 173px;
+    background-color: #E4E4E4;
 
-.FooterC{
-    padding:15px;
+.Footer_main{
+    padding-top:10px;
+    display: flex;
+    justify-content:center;
+    align-items:center;
+    flex-direction:column;
+}
+.hi{
+    display:flex;
+    flex-direction: column;
+}
+.icon_box{
+    font-size:35px;
+    padding-left:20px;
 }
 
-.icon{
-    font-size:40px;
-}
-
-.react{
-    font-family: 'SuncheonB';
-    color:#4fc3f7;
-    font-size:50px;
-}
-
-.Node{
-    font-family: 'SuncheonB';
-    color:#aed581;
-    font-size:50px;
-}
-
-.design{
-    font-family: 'SuncheonB';
-    color:#ffe082;
-    font-size:50px;
+.position{
+    margin: 7px 0px 7px 0px;
 }
 
 .mine{
-    position:relative;
-    top:100px;
+    text-align:right;
+    width:100%;
 }
 
+a{
+    color:black;
+    text-decoration: none;
+}
+
+.icon{
+    font-size:20px;
+}
+
+@media screen and (max-width: 768px) {
+    height: 182px;
+
+    .mine{
+        text-align: center;
+        margin-top:5px;
+    }
+
+}
 `
 
 
