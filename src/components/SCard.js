91602356e3e7be6245948d086b4c8254588a1b71
Maiowol <103277726+Maiowol@users.noreply.github.com>
2022-06-30 19:58:08 +0900

    css 수정

--- src/components/SCard.js
+++ src/components/SCard.js
@@ -1,30 +1,35 @@
 import React from 'react'
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+
 import { RecruitData } from '../shared/recruitdata';
+
 
 function SCard() {
 
+    const navigate = useNavigate();
+
     return (
         <>
-        <Container>
-            {RecruitData.map(item => (
-                <div className='card'>
-                    <div className='card-top'>
-                        <p>모집완료</p>
-                        </div>
-                        <div className='title'>
-                             <h1>{item.title}</h1>
-                        </div>
-                        <div className='card-bottom'>
-                            <p>{item.createdAt}</p>
-                            <p>{item.time}</p>
-                            <p>{item.place}</p>
-                            <p>{item.age}</p>
-                        </div>
+            <Container>
+                {RecruitData.map(item => (
                     
-                </div>
-            ))}
-        </Container>
+                        <div className='card'>
+                            <div className='card-top'>
+                                <p>모집완료</p>
+                            </div>
+                            <div className='title'>
+                                <h1>{item.title}</h1>
+                            </div>
+                            <div className='card-bottom'>
+                                <p>{item.createdAt}</p>
+                                <p>{item.time}</p>
+                                <p>{item.place}</p>
+                                <p>{item.age}</p>
+                            </div>
+                        </div>
+                ))}
+            </Container>
         </>
     )
 }
@@ -39,6 +44,7 @@ width: 100%;
 
 
 .card {
+height: 100%;
 background: white;
 border-radius: 30px;
 border: 1px solid lightgray;
@@ -53,10 +59,13 @@ p {
 
 .card-top {
     margin: 40px 0px 0px 30px;
+    width: 100%;
+
 }
 
 .title {
     margin: 30px 10px 30px 33px;
+
 }
 
 .card-bottom {
