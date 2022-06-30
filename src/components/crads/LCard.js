c140ed96cb16d42e38d68ff37d5059928eb6fecc
Maiowol <103277726+Maiowol@users.noreply.github.com>
2022-06-30 20:55:57 +0900

    파일 경로 수정

--- src/components/crads/LCard.js
+++ src/components/crads/LCard.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components';
-import { PlaceData } from '../shared/placedata';
+import { PlaceData } from '../../shared/placedata';
 
 function LCard() {
 
