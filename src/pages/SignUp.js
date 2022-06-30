f2e2e0a82627d4dff18d1e064f3dd95577928ee8
Maiowol <103277726+Maiowol@users.noreply.github.com>
2022-06-30 09:20:54 +0900

    그리드 경로 수정

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,6 +1,6 @@
 import React,{useRef, useState} from 'react'
 import styled from 'styled-components';
-import Grid from '../elements/Grid';
+import Grid from '../components/elements/Grid';
 import { RiKakaoTalkFill } from 'react-icons/ri';
 import { Navigate } from 'react-router-dom';
 import axios from "axios";
