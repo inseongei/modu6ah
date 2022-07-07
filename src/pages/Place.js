438d8c6f6f86dbb9d2be65a497d310a7101b5199
Maiowol <103277726+Maiowol@users.noreply.github.com>
2022-07-07 09:20:22 +0900

    수정 중

--- src/pages/Place.js
+++ src/pages/Place.js
@@ -4,25 +4,28 @@ import styled from 'styled-components';
 //  elements & components
 import Grid from '../components/elements/Grid';
 import Header from '../components/Header';
+import LCard from '../components/cards/LCard';
 
 function Place() {
   return (
     <div>
-      <Header/>
+      <Header />
       <Grid align="center" height="100px" margin="0 0 32 0">
-           <Title>장소 추천 게시글</Title>
-           </Grid>
-        {/* <Container>
-       <div className='cards'>
-         <SCard />
-       </div>
-      </Container> */}
+        <Title>장소 추천 게시글</Title>
+      </Grid>
+      <Container>
+        <div className='cards'>
+          <LCard />
+        </div>
+      </Container>
     </div>
   )
 }
 
-const Title = styled.h1 `
+const Title = styled.h1`
 margin-top: 50px;
 `;
+
+const Container = styled.div``;
 
 export default Place
