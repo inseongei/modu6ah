bdfcbad00aee36cad14d85931fd0027f69a4ff36
Maiowol <103277726+Maiowol@users.noreply.github.com>
2022-07-07 09:19:41 +0900

    수정 중

--- src/pages/Review.js
+++ src/pages/Review.js
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 //  elements & components
 import Grid from '../components/elements/Grid';
 import Header from '../components/Header';
+import RCard from '../components/cards/RCard';
 
 function Place() {
   return (
@@ -12,11 +13,11 @@ function Place() {
       <Grid align="center" height="100px" margin="0 0 32 0">
            <Title>육아 물품 추천</Title>
            </Grid>
-        {/* <Container>
+        <Container>
        <div className='cards'>
-         <SCard />
+         <RCard />
        </div>
-      </Container> */}
+      </Container>
     </div>
   )
 }
@@ -24,5 +25,7 @@ function Place() {
 const Title = styled.h1 `
 margin-top: 50px;
 `;
+
+const Container = styled.div``;
 
 export default Place;
