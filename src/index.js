116da6aa85d12548ee87b0c9a5056f110158cd38
Maiowol <103277726+Maiowol@users.noreply.github.com>
2022-07-09 20:03:14 +0900

    스크롤 오류 해결

--- src/index.js
+++ src/index.js
@@ -6,12 +6,13 @@ import { BrowserRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.css';
 import { Provider } from "react-redux";
 import store from './redux/configStore'
-
+import ScrollToTop from './ScrollToTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <BrowserRouter>
+    <ScrollToTop />
     <App />
   </BrowserRouter>
   </Provider>
