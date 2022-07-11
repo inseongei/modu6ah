eec138498cf2004f2a18dbc87ece9cd06a0f2bb5
inseong <twin2688@naver.com>
2022-07-11 21:32:19 +0900

    최신본

--- src/shared/firebase.js
+++ src/shared/firebase.js
@@ -1,17 +1,17 @@
 import { initializeApp } from 'firebase/app';
 import {getAuth} from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import {getStorage} from 'firebase/storage';
+import {getFirestore} from 'firebase/firestore';
+import {getStorage} from "firebase/storage";
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAQAHZ1tgFm1oenjQQrH24sMZp4HgSFqm0",
-    authDomain: "clone-5d962.firebaseapp.com",
-    projectId: "clone-5d962",
-    storageBucket: "clone-5d962.appspot.com",
-    messagingSenderId: "309014682499",
-    appId: "1:309014682499:web:99dbad2dbba1678a97f4b6",
-    measurementId: "G-SZQ1N4M2HZ"
+  apiKey: "AIzaSyApS390h7qbe8rji-j8xe6dv4-BfNEOYL4",
+  authDomain: "ins-magazine.firebaseapp.com",
+  projectId: "ins-magazine",
+  storageBucket: "ins-magazine.appspot.com",
+  messagingSenderId: "214654099482",
+  appId: "1:214654099482:web:fb8e99c5b56cad0cb831a3",
+  measurementId: "G-ZGLVNQMSMS"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -19,4 +19,5 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+
 export default app;
