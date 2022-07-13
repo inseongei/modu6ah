db5c2a01dc892f772bd68df1ad13f8430c64d4c6
Maiowol <103277726+Maiowol@users.noreply.github.com>
2022-07-14 00:42:48 +0900

    .

---
+++ src/shared/kakaoData.js
@@ -0,0 +1,4 @@
+export const REST_API_KEY = 'c5bc309928f794ef2cd0c4dde718d8e5';
+export const REDIRECT_URI = 'http://localhost:3000/api/users/kakao/callback'
+
+export const MAP_KEY = '5b4fe67b6e1db979b42554fbdfcd4489';
