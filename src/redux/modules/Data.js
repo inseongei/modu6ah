390282132169a9b0fabd91c27d60a917c02b50cd
inseong <twin2688@naver.com>
2022-07-14 20:15:02 +0900

    최신본 20:15

--- src/redux/modules/Data.js
+++ src/redux/modules/Data.js
@@ -1,42 +1,63 @@
 // Data.js   모듈 안에 있는 리듀서를 묶어서 스토어를 만드는 것
-import axios from 'axios';
-import { getCookie } from '../../shared/Cookie'
+import axios from "axios";
+import { getCookie } from "../../shared/Cookie";
 import io from "socket.io-client";
 
-const socket = io.connect("http://13.125.241.180")
+const socket = io.connect("http://13.125.241.180");
 
-// Actions      --> 저장변수 = 프로젝트명 / 모듈 명 (리듀서 명) / 액션 
-const GET = 'MyPage/GET'
+// Actions      --> 저장변수 = 프로젝트명 / 모듈 명 (리듀서 명) / 액션
+const GET = "MyPage/GET";
+const ProfileGET = "Profile/GET";
 
 // State 초기값
-let initialState ={
-}
+let initialState = {};
 
 // Action Creators      --> 액션 생성 함수
-export function GetMyPage(MyPage){
-    return{type:GET,MyPage}
+export function GetMyPage(MyPage) {
+  return { type: GET, MyPage };
 }
 
-// middleware --> 미들웨어 / 
-export const GetMyPageAxios = () =>{
-    return function (dispatch){
-        axios.get('http://13.125.241.180/api/mypage',{ headers : { Authorization: `Bearer ${getCookie("accessToken")}`}})
-        .then((res)=>{
-            dispatch(GetMyPage(res.data))
-        }).catch((err)=>{
-            console.log(err)
-        })
-    }
+export function GetProfile(Profile) {
+  return { type: ProfileGET, Profile };
 }
 
+// middleware --> 미들웨어 /
+export const GetMyPageAxios = (nickname) => {
+  return function (dispatch) {
+    axios
+      .get("http://dlckdals04.shop/api/mypage/profile/" + nickname, {
+        headers: { Authorization: `Bearer ${getCookie("accessToken")}` },
+      })
+      .then((res) => {
+        dispatch(GetMyPage(res.data));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+};
 
+// export const GetProfileAxois = (nickname) => {
+//   return function (dispatch) {
+//     axios
+//       .get("http://dlckdals04.shop/api/mypage/profile/" + nickname)
+//       .then((res) => {
+//         dispatch(GetProfile(res.data));
+//         console.log(res);
+//       });
+//   };
+// };
 
 // Reducer
-export default function Data(state = initialState , action= {}){
-    switch (action.type){
-        case 'MyPage/GET' : {
-            return {state:action.MyPage}
-        }
-        default: return state;
+export default function Data(state = initialState, action = {}) {
+  switch (action.type) {
+    case "MyPage/GET": {
+      return { state: action.MyPage };
     }
-} 
+    case "Profile/GET": {
+      return { Profile: action.Profile };
+    }
+    default:
+      return state;
+  }
+}
