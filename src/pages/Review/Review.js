fcc5b9e05f7658b42815004149c68f2c87102ca7
Maiowol <103277726+Maiowol@users.noreply.github.com>
2022-07-17 23:22:39 +0900

    icon 추가

--- src/pages/Review/Review.js
+++ src/pages/Review/Review.js
@@ -1,113 +1,136 @@
-import React from 'react'
-import styled from 'styled-components';
+//육아템 리뷰 
+import React from "react";
+import styled from "styled-components";
 
 //  elements & components
-import Grid from '../../components/elements/Grid';
-import Header from '../../components/main/Header';
-import Footer from '../../components/main/Footer';
-import RCard from '../../components/cards/RCard';
+import Grid from "../../components/elements/Grid";
+import Header from "../../components/main/Header";
+import Footer from "../../components/main/Footer";
+import RCard from "../../components/cards/RCard";
+import bottle from '../../images/bottle.png';
 
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 function Review() {
+  const token = localStorage.getItem("accessToken");
   const navigate = useNavigate();
   return (
     <div>
-      <Header/>
+      <Header />
       <Container>
-      <TitleBox>
-      <Title>육아템 리뷰</Title>
-            <div className='subtitle'>
-            <SubTitle>
-            유용한 육아 아이템들을 소개하고 추천해요!
-            </SubTitle>
-            </div>
-            <span>내가 써본 육아템, 알려주고 싶다면?</span>
-            <div className='button' >
-            <Btn onClick={() => 
-            { navigate(`/reviewadd`) }}
-            >
-              후기 작성하기</Btn>
-              </div>
-          </TitleBox>
-          <div className='card_box'>
-          <RCard />
+        <TitleBox>
+          <Title>육아템 리뷰</Title>
+          <div className="subtitle">
+            <SubTitle>유용한 육아 아이템들을 소개하고 추천해요!</SubTitle>
           </div>
+          <div className="image">
+          <img src={bottle}/>
+          </div>
+          <span>내가 써본 육아템, 알려주고 싶다면?</span>
+          <div className="button">
+            {!token ? (
+              <Btn
+                onClick={() => {
+                  navigate(`/login`);
+                }}
+              >
+                후기 작성하기
+              </Btn>
+            ) : (
+              <Btn
+                onClick={() => {
+                  navigate(`/reviewadd`);
+                }}
+              >
+                후기 작성하기
+              </Btn>
+            )}
+          </div>
+        </TitleBox>
+        <div className="card_box">
+          <RCard />
+        </div>
       </Container>
-      <Footer/>
+      <Footer />
     </div>
-  )
+  );
 }
 
 const Container = styled.div`
-font-family: 'Nanum Gothic';
+  font-family: "Nanum Gothic";
 
-.subtitle {
-  display: flex;
-  justify-content: center;
-  align-items:center;
+  .subtitle {
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
-  
+
   span {
-  display: flex;
-  justify-content: center;
-  align-items:center;
-  margin-bottom: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20px;
+    font-weight: 700;
   }
-  
+
   .card_box {
     padding-top: 70px;
-    background-color: #F5F5F5;
+    background-color: #f5f5f5;
   }
 
   .button {
     display: flex;
     justify-content: center;
-    align-items:center;
+    align-items: center;
     margin-bottom: 20px;
-    }
+  }
 `;
 
 const TitleBox = styled.div`
-background-color: white;
-padding-top: 50px;
-padding-bottom: 70px;
+  background-color: white;
+  padding-top: 50px;
+  padding-bottom: 70px;
+
+  .image {  
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20px;
+  } 
 `;
 
-
 const Title = styled.p`
-display: flex;
-justify-content: center;
-align-items:center;
-font-size: 35px;
-font-weight: 750;
-margin-bottom: 0px;
-color: #6B4E16;
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 35px;
+  font-weight: 750;
+  margin-bottom: 0px;
+  color: #6b4e16;
+`;
 
 const SubTitle = styled.p`
-font-size: 20px;
-font-weight: 700;
-line-height: 29px;
-color: #3C3C3C;
-margin-bottom: 50px;
-padding-top: 15px;
- `;
+  font-size: 20px;
+  font-weight: 700;
+  line-height: 29px;
+  color: #3c3c3c;
+  margin-bottom: 20px;
+  padding-top: 15px;
+`;
 
 const Btn = styled.button`
-height: 50px;
-font-size: 15px;
-width: 330px;
-color: #ffffff;
-background-color: #3C3C3C;
-text-align: center;
-border-radius: 30px;
-touch-action: manipulation;
-justify-content: center;
-align-items: center;
-border: 1px solid transparent;
-cursor: pointer;
- `;
-
+  height: 50px;
+  font-size: 15px;
+  width: 330px;
+  color: #ffffff;
+  background-color: #3c3c3c;
+  text-align: center;
+  border-radius: 30px;
+  touch-action: manipulation;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid transparent;
+  cursor: pointer;
+`;
 
 export default Review;
