464ee86022558c01e75e2dac43b5a5c812202bc7
Maiowol <103277726+Maiowol@users.noreply.github.com>
2022-07-21 03:28:12 +0900

    css 수정

--- src/components/cards/RCard.js
+++ src/components/cards/RCard.js
@@ -6,11 +6,14 @@ import { MdOutlinePlace } from "react-icons/md";
 import { useNavigate } from 'react-router-dom';
 import dog from '../../images/dog.jpg'
 import axios from 'axios'
+import { BsBookmark, BsFillBookmarkFill } from "react-icons/bs";
 
 
 function RCard() {
-    const navigate = useNavigate();
-    const [Detail,setDetail] = React.useState()
+const navigate = useNavigate();
+const [Detail,setDetail] = React.useState()
+const Profile = localStorage.getItem("profileUrl");
+
     React.useEffect(()=>{
         axios.get('http://dlckdals04.shop/api/reviews')
         .then((res)=>{
@@ -20,41 +23,48 @@ function RCard() {
     },[])
     return (
         <Container>
-            {Detail&&Detail.map((item, index) => (
-                <div className='card'
-                    key={index}
-                    onClick={() => {
-                        navigate('/reviewdetail/'
-                            + item.reviewPostId
-                        )
-                    }}>
+        {Detail&&Detail.map((item, index) => (
+            <div className='card'
+                key={index}>
 
-                    {/* 카드 위쪽 '타이틀' */}
-                    <div className='card-top'>
-                        <h3>{item.title}</h3>
-                        <p>{item.productType}</p>
+                {/* 카드 위쪽 '타이틀' */}
+                <div className='card-top'>
+                    <div>
+                    <h3>{item.title}</h3>
+                    <p>{item.productType}</p>
                     </div>
-                    <a><MdOutlinePlace /> {item.url}</a>
-                    {/* 카드 중간 '이미지'*/}
-                    <div className='card-body'>
-                        <div className='image'>
-                            <img src={item.imageUrl[0]} alt="사진" />
+
+                    <div>
+                        <BsBookmark className='bookmark'/>
+                    </div>
+
+                </div>
+                <a><MdOutlinePlace /> {item.url}</a>
+                {/* 카드 중간 '이미지'*/}
+                <div className='card-body' 
+                onClick={() => {
+                    navigate('/reviewdetail/'
+                        + item.reviewPostId
+                    )
+                }}>
+                    <div className='image'>
+                        <img src={item.imageUrl[0]} alt="사진" />
+                    </div>
+                    {/* 카드 아래쪽 '아이디 및 내용물' */}
+                    <div className='profile_box'>
+                        <div className='detail_profile'>
+                            <img src={Profile} alt="프로필" />
                         </div>
-                        {/* 카드 아래쪽 '아이디 및 내용물' */}
-                        <div className='profile_box'>
-                            <div className='detail_profile'>
-                                <img src={item.profileUrl} alt="프로필" />
-                            </div>
-                            <strong>{item.nickname}</strong>
-                        </div>
-                        <div className='content'>
-                            <p>{item.content}</p>
-                        </div>
+                        <strong>{item.nickname}</strong>
+                    </div>
+                    <div className='content'>
+                        <p>{item.content}</p>
                     </div>
                 </div>
-            ))}
-        </Container>
-    )
+            </div>
+        ))}
+    </Container>
+)
 }
 
 const Container = styled.div`
@@ -63,78 +73,106 @@ grid-template-columns: repeat(auto-fit, 460px);
 gap: 3.6em;
 justify-content: center;
 align-items: center;
+
 .card {
 background: white;
 border-radius: 30px;
 border: none;
 box-shadow: 0 0 30px 0 rgba(0, 0, 0, 0.17);
-cursor: pointer;
 overflow: hidden;
 height: 570px;
 }
+
 .card-top {
-    display: flex;
-    margin: 30px 0px 0px 50px;
-    h3{
-        font-weight: 700;
-    }
+display: flex;
+justify-content: space-between;
+margin: 30px 0px 0px 50px;
+h3{
+    font-weight: 700;
 }
+}
+
 .card-top p {
- display: flex;
- margin-top: 8px;
- margin-left: 10px;
- color: gray;
- 
+display: flex;
+margin-top: 8px;
+margin-left: 10px;
+color: gray;
 }
+
+.card-top > div {
+display: flex;
+}
+
+.bookmark{
+margin-right: 60px;
+width: 34px;
+height: 34px;
+cursor: pointer;
+}
+
 a {
-    margin-left: 51px;
+margin-left: 51px;
 }
+
 .card-body {
-    width: 100%;
+width: 100%;
+cursor: pointer;
+text-align: center;
 }
+
 .image{
-    border-radius: 25px;
-    overflow: hidden;
+border-radius: 25px;
+overflow: hidden;
 }
+
 .card-body img {
-    width: 90%;
-    height: 270px;
-    margin-top: 3px;
-    object-fit: cover;
+width: 80%;
+height: 270px;
+margin-top: 3px;
+object-fit: cover;
+border-radius: 25px;
 }
+
 .profile_box{
-    display: flex;
-    margin-top: 15px;
+display: flex;
+margin-top: 15px;
+padding-left:30px;
 }
-  .detail_profile > img {
-    width:45px;
-    height:45px;
-    border-radius:50%;
-    margin-left: 10px;
+.detail_profile > img {
+width:45px;
+height:45px;
+border-radius:50%;
+margin-left: 10px;
 }
+
 .detail_profile{
-    border-radius:50%;
-    /* display:flex; */
-    align-items:center;
-    display:block;
-    justify-content:center;
+border-radius:50%;
+/* display:flex; */
+align-items:center;
+display:block;
+justify-content:center;
 }
-  
-  strong { 
-    font-family: 'Noto Sans KR';
-    margin-top: 12px;
-    margin-left: 10px;
-  }
-  .card-body p {
-    margin-top: 10px;
-    margin-left: 5px;
-  }
-  .content { 
-    width: 330px;
-    height: 80px;
-    box-sizing: border-box;
-    overflow: hidden;
+
+strong { 
+font-family: 'Noto Sans KR';
+margin-top: 12px;
+margin-left: 10px;
+}
+
+.card-body p {
+margin-top: 10px;
+margin-left: 5px;
+}
+
+.content { 
+width: 100%;
+height: 80px;
+box-sizing: border-box;
+overflow: hidden;
+margin-top: 10px;
+padding-left:30px;
+text-align:left;
 }
 `;
 
-export default RCard
+export default RCard;
