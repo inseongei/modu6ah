ff7923e8e1c111e8772c37573f6932044be7212d
inseong <twin2688@naver.com>
2022-07-21 22:56:25 +0900

    북마크 끝

--- src/components/cards/LCard.js
+++ src/components/cards/LCard.js
@@ -1,167 +1,258 @@
 //  장소 추천 카드
-import React, {useState} from 'react'
-import styled from 'styled-components';
-import axios from 'axios';
+import React, { useState } from "react";
+import styled from "styled-components";
+import axios from "axios";
 import { MdOutlinePlace } from "react-icons/md";
-import { useNavigate } from 'react-router-dom';
-import dog from '../../images/dog.jpg'
-import { useDispatch, useSelector } from 'react-redux';
-import { loadPhotoDB } from '../../redux/modules/placepage';
+import { useNavigate } from "react-router-dom";
+import dog from "../../images/dog.jpg";
+import { useDispatch, useSelector } from "react-redux";
+import { loadPhotoDB } from "../../redux/modules/placepage";
+import { BsBookmark, BsFillBookmarkFill } from "react-icons/bs";
 
 function LCard() {
- const [data, setData] = useState('');
- const Profile = localStorage.getItem("profileUrl");
+  const [data, setData] = useState("");
+  const Profile = localStorage.getItem("profileUrl");
+  const navigate = useNavigate();
 
- const navigate = useNavigate();
-
- React.useEffect(()=>{
-    axios.get('http://dlckdals04.shop/api/places', {
-         Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+  const token = localStorage.getItem("accessToken");
+  React.useEffect(() => {
+    axios
+      .get("http://dlckdals04.shop/api/places", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
       })
-    .then((res)=>{
-        // console.log(res.data)
-        setData(res.data.placePosts)
-    })
-},[])
+      .then((res) => {
+        // console.log(res.data);
+        setData(res.data.placePosts);
+      });
+  }, []);
 
-    return (
-        <>
-            <Container>
-                {data && data.map ((item, index) => (
-                    <div className='card' 
-                    key={index}
+  return (
+    <>
+      <Container>
+        {data &&
+          data.map((item, index) => (
+            <div className="card" key={index}>
+              {/* 카드 왼쪽 '이미지' */}
+              <div
+                className="card-left"
+                onClick={() => {
+                  navigate("/placedetail/" + item.placePostId);
+                }}
+              >
+                <div className="image">
+                  <img src={item.imageUrl[0]} alt="사진" />
+                </div>
+              </div>
+              {/* 카드 오른쪽 '타이틀 및 설명' */}
+              <div className="card-right">
+                <div className="title">
+                  <div
+                    className="titleBox"
                     onClick={() => {
-                        navigate('/placedetail/' 
-                        + item.placePostId
-                        )
-                    }}>
-                        {/* 카드 왼쪽 '이미지' */}
-                        <div className='card-left'>
-                            <div className='image'>
-                                <img src={item.imageUrl[0]} />
-                            </div>
+                      navigate("/placedetail/" + item.placePostId);
+                    }}
+                  >
+                    <h3>{item.title}</h3>
+                    <p>⭐ {item.star}</p>
+                  </div>
 
-                        </div>
-                        {/* 카드 오른쪽 '타이틀 및 설명' */}
-                        <div className='card-right'>
-                            <div className='title'>
-                                <h3>{item.title}</h3>
-                                <p>⭐ {item.star}</p>
-                            </div>
-                            <a><MdOutlinePlace/>{item.region}</a>
-                            <div className='profile_box'>
-                            <div className='detail_profile'>
-                                <img 
-                                src={Profile}
-                                 alt="프로필" />
-                            </div>
-                                <strong>{item.nickname}</strong>
-                            </div>
-                            <div className='content'>
-                                <p>{item.content}</p>
-                            </div>
-
-                        </div>
-                    </div>
-                ))}
-            </Container>
-        </>
-    )
+                  <div>
+                    {item.bookmarkStatus === true ? (
+                      <BsFillBookmarkFill
+                        className="bookmark2"
+                        onClick={() => {
+                          axios
+                            .put(
+                              "http://dlckdals04.shop/api/places/bookmark/" +
+                                item.placePostId,
+                              null,
+                              {
+                                headers: {
+                                  Authorization: `Bearer ${localStorage.getItem(
+                                    "accessToken"
+                                  )}`,
+                                },
+                              }
+                            )
+                            .then((res) => {
+                              console.log(res);
+                              window.location.reload();
+                            });
+                        }}
+                      />
+                    ) : (
+                      <BsBookmark
+                        className="bookmark"
+                        onClick={() => {
+                          axios
+                            .put(
+                              "http://dlckdals04.shop/api/places/bookmark/" +
+                                item.placePostId,
+                              null,
+                              {
+                                headers: {
+                                  Authorization: `Bearer ${localStorage.getItem(
+                                    "accessToken"
+                                  )}`,
+                                },
+                              }
+                            )
+                            .then((res) => {
+                              console.log(res);
+                              window.location.reload();
+                            });
+                        }}
+                      />
+                    )}
+                  </div>
+                </div>
+                <a href="##" className="atag">
+                  <MdOutlinePlace />
+                  {item.region}
+                </a>
+                <div
+                  className="profile_box"
+                  onClick={() => {
+                    navigate("/placedetail/" + item.placePostId);
+                  }}
+                >
+                  <div className="detail_profile">
+                    <img src={Profile} alt="프로필" />
+                  </div>
+                  <strong>{item.nickname}</strong>
+                </div>
+                <div
+                  className="content"
+                  onClick={() => {
+                    navigate("/placedetail/" + item.placePostId);
+                  }}
+                >
+                  <p>{item.content}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+      </Container>
+    </>
+  );
 }
 
 const Container = styled.div`
-font-family: 'Nanum Gothic';
+  font-family: "Nanum Gothic";
 
-display: grid;
-grid-template-columns: repeat(auto-fit);
-gap: 3.5em;
-justify-content: center;
-align-items: center;
+  display: grid;
+  grid-template-columns: repeat(auto-fit);
+  gap: 3.5em;
+  justify-content: center;
+  align-items: center;
 
+  .card {
+    background: white;
+    border-radius: 30px;
+    border: none;
+    box-shadow: 0 0 30px 0 rgba(0, 0, 0, 0.17);
+    overflow: hidden;
+    width: 980px;
+    height: 360px;
+    display: flex;
+    flex-direction: row;
+  }
 
-.card {
-background: white;
-border-radius: 30px;
-border: none;
-box-shadow: 0 0 30px 0 rgba(0, 0, 0, 0.17);
-cursor: pointer;
-overflow: hidden;
-width: 980px;
-height: 360px;
-display: flex;
-flex-direction: row;
-}
-
-.card-left {
+  .card-left {
     display: flex;
     width: 460px;
     height: 300px;
     margin: 30px 0px 0px 39px;
     padding-top: 10px;
     padding-bottom: 10px;
-}
+    cursor: pointer;
+  }
+  .bookmark2 {
+    margin-right: 60px;
+    width: 34px;
+    height: 34px;
+    color: #6b4e16;
+    cursor: pointer;
+  }
+  .titleBox {
+    cursor: pointer;
+  }
 
-.image {
+  .atag {
+    text-decoration: none;
+    color: black;
+    margin: 20px 0px 20px 0px;
+  }
+  .bookmark {
+    margin-right: 60px;
+    width: 34px;
+    height: 34px;
+    cursor: pointer;
+  }
+  .image {
     border-radius: 25px;
     width: 100%;
     overflow: hidden;
-}
+  }
 
-.card-left img {
+  .card-left img {
     width: 100%;
     height: 100%;
     object-fit: cover;
-}
+  }
 
-.card-right {
+  .card-right {
     display: flex;
     flex-direction: column;
     margin-top: 40px;
     margin-left: 60px;
-}
+  }
 
-.title {
+  .title {
     display: flex;
-    
-    h3{
-        font-weight: 700;
-    }
-}
+    justify-content: space-between;
 
-.title p {
+    h3 {
+      font-weight: 700;
+    }
+  }
+
+  .title p {
     margin-top: 4px;
     margin-left: 10px;
-}
+  }
 
-.profile_box{
+  .profile_box {
     display: flex;
     margin-top: 15px;
     margin-bottom: 20px;
-}
+    cursor: pointer;
+  }
 
-.profile{
-    width:50px;
-    height:50px;
-    border-radius:50%;
-    border:1px solid black;  
+  .profile {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    border: 1px solid black;
   }
 
   .detail_profile > img {
-    width:45px;
-    height:45px;
-    border-radius:50%;
+    width: 45px;
+    height: 45px;
+    border-radius: 50%;
     margin-left: 10px;
-}
+  }
 
-.detail_profile{
-    border-radius:50%;
-    /* display:flex; */
-    align-items:center;
-    display:block;
-    justify-content:center;
-}
-  
-  strong { 
+  .detail_profile {
+    border-radius: 50%;
+    align-items: center;
+    display: block;
+    justify-content: center;
+  }
+
+  strong {
     margin-top: 10px;
     margin-left: 10px;
   }
@@ -170,18 +261,18 @@ flex-direction: row;
     margin: 0px 10px 0px 5px;
   }
 
-  .content { 
-      margin-right: 20px;
-      width: 420px;
-      height: 180px;
-      box-sizing: border-box;
-      overflow: hidden;
+  .content {
+    margin-right: 20px;
+    width: 420px;
+    height: 180px;
+    box-sizing: border-box;
+    overflow: hidden;
+    cursor: pointer;
   }
 
   .content p {
-      font-weight: normal;
+    font-weight: normal;
   }
-
 `;
 
 export default LCard;
