0f9ad586fc105ac51139ac8b9d006100fbfff1a7
Maiowol <103277726+Maiowol@users.noreply.github.com>
2022-07-28 23:26:21 +0900

    footer

--- src/components/main/Footer.js
+++ src/components/main/Footer.js
@@ -65,7 +65,7 @@ const FooterContainer = styled.footer`
   height: 130px;
   background-color: #3c3c3c;
   position: relative;
-  transform: translateY(270%);
+  transform: translateY(0%);
   font-family: 'NanumGothic';
 
   .one_container {
